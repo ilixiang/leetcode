4802ab63518d34814dd8da128c7696ea195929b1
ilixiang <ilixiang626@163.com>
2022-06-23 14:49:52 +0800

    041;

--- 041.py
+++ 041.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
 def firstMissingPositive(nums):
-    minPositiveNum = 0
+    for index in range(0, len(nums)):
+        nums[index] = nums[index] - 1
+    for index in range(0, len(nums)):
+        num = nums[index]
+        while num >= 0 and num < len(nums) and num != index and num != nums[num]:
+            tmp = nums[num]
+            nums[num] = num
+            num = tmp
+        nums[index] = num
+    for index in range(0, len(nums)):
+        if index != nums[index]:
+            return index + 1
+    return len(nums) + 1
+
+print(firstMissingPositive([1, 2, 0]))
+print(firstMissingPositive([3, 4, -1, 1]))
+print(firstMissingPositive([7, 8 ,9, 11, 12]))
+print(firstMissingPositive([1, 1]))
 
