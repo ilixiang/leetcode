0ddcbcec5a9490e192099816060b54de85cfeb7e
richard <ilxiang626@163.com>
2022-11-07 02:11:14 +0800

    162

--- v2/162.py
+++ v2/162.py
@@ -4,13 +4,9 @@ def findPeakElement(nums):
     left, right = 0, len(nums) - 1
     while left < right:
         mid = (left + right) >> 1
-        if nums[left] < nums[right]:
+        if nums[mid] < nums[mid + 1]:
             left = mid + 1
-        elif nums[left] > nums[right]:
-            right = mid - 1
         else:
-            if nums[mid] > nums[left]:
-                left = mid
-            else:
-                right = mid - 1
+            right = mid
     return (left + right) >> 1
+
